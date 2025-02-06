5a3e000af83f38c8d094089253cf278dc5c88e08
Pat-TX <williams.patrick.d@gmail.com>
2025-02-06 14:06:35 -0500

    Revert "still working on pages"
    This reverts commit 4196c3f4b3966501b1aafdd160e68fc430a6a2fc.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    output: 'export',
     images: {
-      unoptimized: true, // Disable default image optimization
+      unoptimized: true,
     },
-    output: 'export'
   };
 
 export default nextConfig;
